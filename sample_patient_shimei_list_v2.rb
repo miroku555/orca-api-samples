972fa6f1e38b2b7e7f7d5c85a8a1ed61df8765f6
MirokuNakazawa <nacl201402@gmail.com>
2014-09-03 10:42:30 +0900

    a

---
+++ sample_patient_shimei_list_v2.rb
@@ -0,0 +1,82 @@
+#!/usr/bin/ruby
+# -*- coding: utf-8 -*-
+
+#------ 患者番号一覧取得
+
+
+require 'crack' # for xml and json
+require 'crack/xml' # for just xml
+require 'uri'
+require 'net/http'
+
+Net::HTTP.version_1_2
+
+HOST = "192.168.4.123"
+PORT = "8000"
+USER = "ormaster"
+PASSWD = "ormaster123"
+CONTENT_TYPE = "application/xml"
+
+req = Net::HTTP::Post.new("/api01rv2/patientlst3v2?class=01")
+# class :01 新規・更新対象
+# class :02 新規対象
+#
+#
+BODY = <<EOF
+
+<data>
+  <patientlst3req type="record">
+  <WholeName type="string">#{ARGV[0]}</WholeName>
+  <Contain_TestPatient_Flag type="string">1</Contain_TestPatient_Flag>
+  </patientlst3req>
+</data>
+EOF
+
+def list_patient(body)
+  puts "----------------------------"
+  root = Crack::XML.parse(body)
+
+  resulet= root["xmlio2"]["patientlst2res"]["Api_Result"]
+  #unless result == "00"
+  #  puts "error:#{result}"
+  #  exit 1
+  #end
+  pinfo = root["xmlio2"]["patientlst2res"]["Patient_Information"]
+  pinfo.each do |patient|
+    print"    ID    |"
+    puts patient["Patient_ID"]
+	print"   名前   |"
+    puts patient["WholeName"]
+	print"   カナ　 |"
+	puts patient["WholeName_inKana"]
+	print" 生年月日 |"
+	puts patient["BirthDate"]
+	print"   性別 　|"
+	if patient["Sex"] == "1"
+	  puts "男"
+	else
+	  puts "女"
+	end
+	print"  作成日　|"
+	puts patient["CreateDate"]
+	print"  更新日　|"
+	puts patient["UpdateDate"]
+	puts "--------------------------"
+  end
+end
+
+	
+	req.content_length = BODY.size
+	req.content_type = CONTENT_TYPE
+	req.body = BODY
+	req.basic_auth(USER, PASSWD)
+	
+Net::HTTP.start(HOST, PORT) {|http|
+res = http.request(req)
+  #puts res.code
+  #puts res.body
+
+  list_patient(res.body)
+  
+
+}
